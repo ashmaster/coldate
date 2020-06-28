b860911da9a901491ec65c33cf92ee945b28ce7a
Ashish <ashishmp0471@gmail.com>
2020-06-28 13:26:31 +0530

    Latses

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const userRouter = require('./routers/user')
+const cors = require('cors')
 const port = process.env.PORT || 3000
 require('./db/db')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors)
 app.use(userRouter)
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
