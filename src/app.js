b0da0d83b467ea65baa349b7ef8ecb779bd377e7
Ashish <ashishmp0471@gmail.com>
2020-06-28 13:56:40 +0530

    Latses

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userRouter = require('./routers/user')
 const cors = require('cors')
+const port = process.env.PORT||3000
 require('./db/db')
 
 const app = express()
@@ -8,6 +9,6 @@ const app = express()
 app.use(express.json())
 app.use(cors)
 app.use(userRouter)
-app.listen(process.env.PORT||3000, () => {
-    console.log(`Server running on Heroku`)
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
 })
